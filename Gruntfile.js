dab06562633dd0132d85606610258a1c71140225
bdzevel <bdzevel@gmail.com>
2017-06-24 17:46:58 -0400

    Uncomment line in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    //pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
     eslint: {
       options: { config: '.eslintrc.//js' },
       src: [ 'index.js' ],
